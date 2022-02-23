68ff5617d43443e26a8d31e5b2657f1ea05ee955
James Brouckman <jcbrouck@mtu.edu>
2022-02-23 14:49:01 -0500

    added requests and basic rerouting of courses link

--- schedulerBackend/api/views.py
+++ schedulerBackend/api/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+import requests
 
 # Create your views here.
+def classes(request):
+    return(HttpResponse(requests.get('https://api.michigantechcourses.com/courses')))
