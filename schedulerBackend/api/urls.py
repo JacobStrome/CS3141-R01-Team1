402bb960248e03c66df1a367ffbd4df4a1e02f4a
James Brouckman <jcbrouck@mtu.edu>
2022-02-28 10:01:03 -0500

    Added sections to data returned by classes and cached results

--- schedulerBackend/api/urls.py
+++ schedulerBackend/api/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('classes', views.classes, name='classes'),
+
 ]
--- schedulerBackend/api/views.py
+++ schedulerBackend/api/views.py
@@ -1,7 +1,15 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.cache import cache_page
 import requests
 
+
 # Create your views here.
+@cache_page(60*5)
 def classes(request):
-    return(HttpResponse(requests.get('https://api.michigantechcourses.com/courses')))
+    courses = requests.get('https://api.michigantechcourses.com/courses').json()
+    sections = requests.get('https://api.michigantechcourses.com/sections').json()
+    for course in courses:
+        course['sections'] = [section for section in sections if section['courseId'] == course['id']]
+    
+    return(JsonResponse({"classes" : courses}))
+
