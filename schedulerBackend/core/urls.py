a03bd94ed0360f2af92f1159b802fcec9bfc4422
TRMoosee <therealmoosee@gmail.com>
2022-03-02 15:28:58 -0500

    Added class page view, also changed where the index goes because build/html doesn't exist yet

--- schedulerBackend/core/urls.py
+++ schedulerBackend/core/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Default
     path('', views.index, name='index'),
+    # Class page
+    path('classes', views.classpage, name='classpage'),
 ]
--- schedulerBackend/core/views.py
+++ schedulerBackend/core/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+# def index(request):
+#    return render(request, 'build/index.html')
 def index(request):
-    return(render(request, 'build/index.html'))
+    return HttpResponse("Hello world, you are at our index!")
+
+
+def classpage(request):
+    return HttpResponse("You are now looking at our class page!")
