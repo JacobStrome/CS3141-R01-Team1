495fc4543bd033b1621b12c313e24af1c91213bf
TRMoosee <therealmoosee@gmail.com>
2022-03-03 10:17:48 -0500

    Classpage.html doesn't do what I want, added def classpage to views

--- schedulerBackend/core/views.py
+++ schedulerBackend/core/views.py
@@ -1,13 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 # Create your views here.
-# def index(request):
-#    return render(request, 'build/index.html')
 def index(request):
-    return HttpResponse("Hello world, you are at our index!")
-
+    return(render(request, 'build/index.html'))
 
 def classpage(request):
-    return HttpResponse("You are now looking at our class page!")
+    return(render(request, 'build/classpage.html'))
