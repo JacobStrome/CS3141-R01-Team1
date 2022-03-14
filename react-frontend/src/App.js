d1bf73296ce5c68ff41fa9300f5c8153877ea244
James Brouckman <jcbrouck@mtu.edu>
2022-03-14 16:50:38 -0400

    Started integrating class data into frontend

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,25 +1,43 @@
-import logo from './assets/TSPlogo.png';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import Navbar from './components/navbar'
 import SearchResults from './components/searchResults'
 import Calendar from './components/calendar'
+import React from 'react';
+import axios from 'axios';
+export class App extends React.Component{
 
-function App() {
-  return (
-    <div className="App">
+  constructor(props){
+    super(props)
+    this.state = {courses: []}
+  }
+
+  componentDidMount(){
+    axios.defaults.headers.get = 'Access-Control-Allow-Origin: *'
+    axios.get('http://127.0.0.1:8000/api/classes').then((response) => {
+      
+      this.setState({
+      courses : response.data
+      })
+    });
+    
+  }
+  render(){
+    return(
+      <div className="App">
       <body>
         {/*navbar*/}
         <Navbar/>
         <div class="row container p-4">
           {/*Div for class search results*/}
-          <SearchResults/>
+          <SearchResults courses = {this.state.courses}/>
+
           {/*div for calendar*/}
           <Calendar/>
         </div>
       </body>
     </div>
-  );
+    )
+  }
 }
-
 export default App;
--- react-frontend/src/components/searchResults.jsx
+++ react-frontend/src/components/searchResults.jsx
@@ -1,37 +1,48 @@
 import React from "react"
-
+import '../App.css'
 
 export default class SearchResults extends React.Component {
 
 
     render(){
-        return(
-            <div class="col-4 classes-div">
+      var tableRows = Object.keys(this.props.courses).map((key, index) =>{
+        <tr>
+          <classRow class={this.props.courses[key]}/>
+        </tr>
+      })
+      console.log(tableRows)
+      return(
+          <div class="col-4 classes-div">
+          <table>
+            <tr>
+              <th class="no-border gray-label"> Classes </th>
+            </tr>
+            {tableRows}
+            <tr>
+              <td class="table-border">cs3xxx</td>
+            </tr>
+            <tr>
+              <td class="table-border">cs3xxx</td>
+            </tr>
+            <tr>
+              <td class="table-border">cs3xxx</td>
+            </tr>
+            <tr>
+              <td class="table-border">cs3xxx</td>
+            </tr>
+            <tr>
+              <td class="table-border">cs3xxx</td>
+            </tr>
+          </table>
+        </div>
+      )
+    }
+}
 
-            <table>
-              <tr>
-                <th class="no-border gray-label"> Classes </th>
-              </tr>
-              <tr>
-                <td class="table-border">cs3xxx</td>
-              </tr>
-              <tr>
-                <td class="table-border">cs3xxx</td>
-              </tr>
-              <tr>
-                <td class="table-border">cs3xxx</td>
-              </tr>
-              <tr>
-                <td class="table-border">cs3xxx</td>
-              </tr>
-              <tr>
-                <td class="table-border">cs3xxx</td>
-              </tr>
-              <tr>
-                <td class="table-border">cs3xxx</td>
-              </tr>
-            </table>
-          </div>
-        )
+
+class classRow extends React.Component{
+
+    render(){
+        return(<td class="table-border">{this.props.courses.name}</td>)
     }
 }
