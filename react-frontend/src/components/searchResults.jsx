0c609da362b718b4af96668c530a2259500928a2
James Brouckman <jbrouckman@gmail.com>
2022-03-20 22:06:16 -0400

    Sorted Courses by number and subject name and added semester filtering

--- react-frontend/src/components/searchResults.jsx
+++ react-frontend/src/components/searchResults.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import './searchResults.css'
-import {IconButton, Paper, TableCell, TableContainer, TableHead, TableRow, Table, Collapse} from '@mui/material'
+import {IconButton, Paper, TableCell, TableContainer, TableHead, TableRow, Table, FormControl, Select, InputLabel, MenuItem, Menu} from '@mui/material'
 import { KeyboardArrowUp, KeyboardArrowDown} from '@mui/icons-material'
 class SubTable extends React.Component{
   constructor(props){
@@ -23,11 +23,14 @@ class SubTable extends React.Component{
             {this.props.subject}
           </TableCell>
           <TableCell/>
+          <TableCell/>
+
         </TableRow>
         {this.state.open && this.props.courses.map((course) =>(
           <TableRow>
+              <TableCell>{course.subject+course.crse}</TableCell>
+              <TableCell>{course.title}</TableCell>
               <TableCell/>
-              <TableCell>{course.subject+course.crse+" " + course.title}</TableCell>
               <TableCell>{course.maxCredits}</TableCell>
           </TableRow>
           
@@ -42,10 +45,26 @@ export default class SearchResults extends React.Component {
       super(props);
 
       this.getRowsFromKeys = this.getRowsFromKeys.bind(this)
+      this.state = {
+        year : 2022,
+        semester : "FALL"
+      }
+    }
+
+    getGivenYears(){
+      var years = []
+      Object.keys(this.props.courses).forEach((key) => {
+        if(!years.includes(this.props.courses[key].year)) years.push(this.props.courses[key].year)
+      })
+      return years;
     }
     getRowsFromKeys(){
       var validCourses = Object.keys(this.props.courses).filter((key)=> {
-        if ((this.props.courses[key].subject+this.props.courses[key].crse +' '+this.props.courses[key].title).toLocaleLowerCase().includes(this.props.searchTerm.toLocaleLowerCase())) return true
+        if ((this.props.courses[key].subject+this.props.courses[key].crse +' '+this.props.courses[key].title)
+              .toLocaleLowerCase().includes(this.props.searchTerm.toLocaleLowerCase())
+             && this.props.courses[key].year == this.state.year 
+             && this.props.courses[key].semester == this.state.semester) return true
+
         else return false
       })
       var subjectDict = {};
@@ -55,28 +74,68 @@ export default class SearchResults extends React.Component {
           subjectDict[course.subject] = []
         subjectDict[course.subject].push(course)
       })
-      var rows = Object.keys(subjectDict).map((key)=>
-        <SubTable subject={key} courses={subjectDict[key]}/>
+      
+      var rows = Object.keys(subjectDict).sort().map((key)=>{
+        subjectDict[key] = subjectDict[key].sort((courseA, courseB) =>{
+          let crseA = parseInt(courseA.crse);
+          let crseB = parseInt(courseB.crse);
+          return crseA - crseB;
+        })
+        return (<SubTable subject={key} courses={subjectDict[key]}/>)
+      }
+        
       )
-      console.log(rows)
+      console.log(validCourses)
+      console.log(this.state.semester)
       return rows
     }
+    
     render(){
       var tableRows = this.getRowsFromKeys()
 
       return(
-        <div className="col-4 classes-div">
-          <TableContainer component={Paper} >
-            <Table>
+        <div className="col-5 classes-div">
+          <TableContainer className="table-container" component={Paper} >
+            <Table stickyHeader>
               <TableHead>
-                <TableRow>
-                  <TableCell/>
+                <TableRow className = "table-head-row">
+                  {Object.keys(this.props.courses).length>0 
+                    ? 
+                    <TableCell>
+                      <FormControl >
+                        <InputLabel>Year</InputLabel>
+                        <Select value={this.state.year} label="Year" onChange={(event) => this.setState({year: event.target.value, semester: this.state.semester})}>
+                          {this.getGivenYears().map((year) => (
+                            <MenuItem value={year}>{year}</MenuItem>
+                          ))
+                          }
+                        </Select>
+                      </FormControl>
+                      
+                    </TableCell>
+                    : <TableCell/>
+                  }
+                  {Object.keys(this.props.courses).length>0 
+                    ?
+                    <TableCell>
+                      <FormControl >
+                        <InputLabel>Semester</InputLabel>
+                        <Select value={this.state.semester} label="Semester" onChange={(event) => this.setState({year: this.state.year, semester: event.target.value})}>
+                            <MenuItem value="FALL">FALL</MenuItem>
+                            <MenuItem value="SPRING">SPRING</MenuItem>
+                            <MenuItem value="SUMMER">SUMMER</MenuItem>
+                        </Select>
+                      </FormControl>
+                    </TableCell>
+                    : <TableCell/>
+                  }
                   <TableCell>Course Title</TableCell>
                   <TableCell align="right"> Credits </TableCell>
                 </TableRow>
               </TableHead>
               {tableRows}
             </Table>
+
           </TableContainer>
         </div>
       )
